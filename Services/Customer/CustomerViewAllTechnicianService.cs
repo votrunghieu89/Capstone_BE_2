d65cfc292f1eb48ae6d8b04e09dab1d3abb65477
votrunghieu89 <votrunghieu4@dtu.edu.vn>
2026-05-04 05:50:52 +0700

    adjust size image

--- Services/Customer/CustomerViewAllTechnicianService.cs
+++ Services/Customer/CustomerViewAllTechnicianService.cs
@@ -231,7 +231,7 @@ namespace Capstone_2_BE.Services.Customer
                     CityId = form.CityId,
                     Latitude = lat,
                     Longitude = lng,
-                    EstimatedTime = 111,
+                    EstimatedTime = 299,
                     ImageOrderUrl = new List<string>(),
                     videoUrl = string.Empty
                 };
